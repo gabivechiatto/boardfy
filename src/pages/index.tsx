b0cf47fbf15fa7296c6876307cf66162312d95f6
Gabriela Vechiatto <vech.gabi@gmail.com>
2023-01-28 14:44:31 -0300

    feat: home revalidation time (static props)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { GetStaticProps } from 'next'
 
 import styles from '../styles/styles.module.scss'
 
@@ -28,4 +29,12 @@ export default function Home() {
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+
+  return {
+    props: {},
+    revalidate: 60 * 60 // 60 min
+  }
 }
